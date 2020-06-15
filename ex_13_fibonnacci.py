41056e342366258048aa78b494be8af6577896bd
Saskia van Barthold <saskia.vanb@gmail.com>
2020-06-15 08:48:18 +0200

    completed ex 13

---
+++ ex_13_fibonnacci.py
@@ -0,0 +1,26 @@
+# n = int(input('How many Fibonnaci numbers do you want to generate: '))
+
+# def fiboncci(n):
+#     new_num, f_num = 0,0
+#     s_num = 1
+#     for i in range(n):
+#         new_num = f_num + s_num
+#         print(f_num)
+#         f_num = s_num
+#         s_num = new_num
+#
+# print(fiboncci(n))
+
+num = int(input('How many Fibonnaci numbers do you want to generate: ')
+
+
+def fibonnacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fibonnacci(n-1) + fibonnacci(n-2)
+
+for n in range(0, num):
+    print(fibonnacci(n))
