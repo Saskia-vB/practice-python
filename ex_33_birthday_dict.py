e1177d226dd60bfc0cfa2ea1a3394ea4f9b64dab
Saskia van Barthold <saskia.vanb@gmail.com>
2020-06-16 08:51:07 +0200

    added birthday dictionary exercise

---
+++ ex_33_birthday_dict.py
@@ -0,0 +1,8 @@
+bday = {"juju": "8/03/1996", "Toto":"30/09/1996", "Dan": "17/05/1995"}
+
+user_input = input("Please enter a name :")
+print(bday[user_input])
+
+# print(bday.keys())
+# print(bday.values())
+# print(bday.items())
