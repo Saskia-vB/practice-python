a99064f5b86adf7d9943ca2dd407ce71c08fcd5c
Saskia van Barthold <saskia.vanb@gmail.com>
2020-06-16 09:09:38 +0200

    completed ex 4

---
+++ ex_4_divisors.py
@@ -0,0 +1,10 @@
+num = int(input("please enter a number :"))
+
+def divisors(num):
+    div_list = []
+    for i in range(1,num):
+        if num % i == 0:
+            div_list.append(i)
+    return div_list
+
+print(divisors(num))
