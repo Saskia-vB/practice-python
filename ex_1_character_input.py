21c268c0596d9ceed0de3923366e0480ea58a90a
Saskia van Barthold <saskia.vanb@gmail.com>
2020-06-16 08:59:36 +0200

    completed ex 1

---
+++ ex_1_character_input.py
@@ -0,0 +1,9 @@
+name =input('please enter your name: ')
+age = int(input("please enter your age: "))
+
+year = ((2020 - age) + 100)
+
+print("Hello",name, "you will turn 100 in", year)
+
+print(f"Hello {name}, you will turn 100 in {year}")
+
